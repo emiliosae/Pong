c219e478b6f71eda4219ee2534e1b17d51f0f364
emiliosae <109813234+emiliosae@users.noreply.github.com>
2022-08-23 10:25:07 -0500

    cambio

--- Assets/Script/Next_level.cs
+++ Assets/Script/Next_level.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Next_level : MonoBehaviour
 {
+    public void Load (string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
     private void OnTriggerExit2D(Collider2D collision)
     {
         Debug.Log("Load Level");
